3782b86de36163711dee6c4e921855fb0881cdee
solutionreparatio <120780001+solutionreparatio@users.noreply.github.com>
2022-12-31 12:40:39 +0100

    last

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload=function()
+{
+var canevas = document.createElement('canevas');
+
+canevas.height=600;
+canevas.width=900;
+canevas.style.border="1px solid";
+document.body.appendChild(canevas);
+
+}
